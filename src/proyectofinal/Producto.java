3239e5ae08d2e8f50dd4f8df77aa50f4d122fb3d
Lis <gallardodiazyessenialisbeth@gmail.com>
2026-06-18 21:56:53 -0500

    ver datos de Producto

---
+++ src/proyectofinal/Producto.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectofinal;
+
+/**
+ *
+ * @author yesse
+ */
+public class Producto {
+    protected String codigo;
+    protected String nombre;
+    protected String categoria;
+    protected double precio;
+    protected int stock;
+
+    public Producto(String codigo, String nombre,
+        String categoria, double precio, int stock) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+    public void verDatos() {
+
+       System.out.println("CODIGO: " + codigo
+                + " NOMBRE: " + nombre
+                + " CATEGORIA: " + categoria
+                + " PRECIO: " + precio
+                + " STOCK: " + stock);
+
+    }
+}
